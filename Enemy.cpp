df33b1087005ab41670ab8b5c868c036ab5a66dc
summerpaigetan <summerpaigetan@gmail.com>
2026-08-18 15:08:33 +0800

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,25 @@
-#include "Enemy.h"
+#pragma once
+#include "Character.h"
+class Enemy : public Character
+{
+	int hp;
+	int attack;
+
+
+public:
+	//Constructor
+	Enemy();
+
+	//Destructor 
+	~Enemy();
+
+	//getter
+	int getHP();
+	int getATK();
+
+	//setter
+	void setHP(int newHP);
+	void setATK(int newAttack);
+
+};
+
