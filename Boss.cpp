f0e4b33feea28dcb8ebf76319d9610bd6a396809
summerpaigetan <summerpaigetan@gmail.com>
2026-08-18 15:07:32 +0800

    Add files via upload

---
+++ Boss.cpp
@@ -0,0 +1,18 @@
+#include "Boss.h"
+
+//Constuctor
+
+//Destructor
+	
+//Getter
+	
+
+//Setter
+	void Boss::setHP(int newHP)
+	{
+
+	}
+	void Boss::setATK(int newATK)
+	{
+
+	}
---
+++ Boss.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Enemy.h"
+class Boss : public Enemy
+{
+	//Constructor
+	Boss();
+
+	//Destructor
+	~Boss();
+
+	//Getter
+	int getHP();
+	int getATK();
+
+	//Setter
+	void setHP(int newHP);
+	void setATK(int newATK);
+
+};
+
