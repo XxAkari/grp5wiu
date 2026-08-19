262b1d6e5ef02df139c8451d76209c801b4837e3
naylisophillea <naylisophillea@gmail.com>
2026-08-19 11:12:22 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,20 +3,9 @@
 
 class Player : public Character {
 public:
+	std::string name;
+	int hp;
+	int atk;
+
 	Player(std::string name, int hp, int attack);
-};
-
-class Fairy : public Player {
-public:
-	Fairy(std::string name);
-};
-
-class Witch : public Player {
-public:
-	Witch(std::string name);
-};
-
-class Assassin : public Player {
-public:
-	Assassin(std::string name);
 };
