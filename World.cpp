fc17b365c080590139de2ef6db6520bd0811a172
naylisophillea <naylisophillea@gmail.com>
2026-08-19 11:14:09 +0800

    Update World.cpp

--- World.cpp
+++ World.cpp
@@ -1,5 +1,6 @@
 #include "World.h"
 #include "Enemy.h"
+#include "Player.h"
 #include <iostream>	
 #include <conio.h>
 
@@ -10,7 +11,44 @@ World::World() {
 		}
 	}
 	credits = 0;
-};
+	curLevel = 1;
+	player = nullptr;
+}
+
+World::~World() {
+	delete player;
+}
+
+void World::startGame() {
+	std::string name = namingUI();
+	std::string pClass = playerClassUI();
+
+	int hp = 0;
+	int atk = 0;
+
+	if (pClass == "Fairy") {
+		hp = 120;
+		atk = 8;
+	}
+	else if (pClass == "Witch") {
+		hp = 80;
+		atk = 12;
+	}
+	else if (pClass == "Assassin") {
+		hp = 100;
+		atk = 15;
+	}
+	else {
+		// Fallback default stats just in case something goes wrong
+		hp = 100;
+		atk = 10;
+	}
+
+	player = new Player(name, hp, atk);
+
+	void printCombatUI(std::string playerName, int pHp, int pAtk, int eHp, int eAtk, int creds);
+
+}
 
 int World::getCredits() {
 	return credits;
@@ -29,21 +67,30 @@ std::string World::namingUI() {
 	std::cout << "Name Your Character: ";
 	std::string playerName;
 	std::cin >> playerName;
-	std::cout << "hello " << playerName << std::endl;
-	std::cout << "Press any key to continue";
+	std::cout << "Hello " << playerName << std::endl;
+	std::cout << "Press any key to continue...";
+	_getch(); 
 	return playerName;
 }
-
 
 std::string World::playerClassUI() {
 	std::string cardname[3] = { "Fairy", "Witch", "Assassin" };
 	std::string carddesc[3] = { "placeholder 1", "placeholder 2", "placeholder 3" };
-	int cardatk[3] = {5, 10, 20};
-	int cardhp[3] = {100, 25, 67};
+	int cardatk[3] = { 5, 10, 20 };
+	int cardhp[3] = { 100, 25, 67 };
 	bool chosen = false;
 	int cardCount = 0;
 
 	while (!chosen) {
+		system("cls"); // Move this to the top of the loop so it draws cleanly on startup
+		std::cout << "The Jimbob Paradox" << std::endl;
+		std::cout << "[ Class Selection ]" << std::endl;
+		std::cout << "Class: " << cardname[cardCount] << std::endl;
+		std::cout << "Desc: " << carddesc[cardCount] << std::endl;
+		std::cout << "Attack: " << cardatk[cardCount] << std::endl;
+		std::cout << "Health: " << cardhp[cardCount] << std::endl;
+		std::cout << "[Z] Previous / [X] Next / [C] Choose" << std::endl;
+
 		char newKey = _getch();
 		switch (newKey) {
 		case 'z':
@@ -58,51 +105,36 @@ std::string World::playerClassUI() {
 			chosen = true;
 			break;
 		}
-		system("cls");
-		std::cout << "The Jimbob Paradox" << std::endl;
-		std::cout << "[ Class Selection ]" << std::endl;
-		std::cout << "Class: " << cardname[cardCount] << std::endl;
-		std::cout << "Desc: " << carddesc[cardCount] << std::endl;
-		std::cout << "Attack: " << cardatk[cardCount] << std::endl;
-		std::cout << "Health: " << cardhp[cardCount] << std::endl;
-		std::cout << "[Z] Previous / [X] Next / [C] Choose" << std::endl;
 	}
 
 	std::string chosenClass = "null";
 	switch (cardCount) {
-	case 0:
-		chosenClass = "Fairy";
-		break;
-	case 1:
-		chosenClass = "Witch";
-		break;
-	case 2:
-		chosenClass = "Assassin";
-		break;
-	default:
-		std::cout << "Invalid Class!" << std::endl;
+	case 0: chosenClass = "Fairy"; break;
+	case 1: chosenClass = "Witch"; break;
+	case 2: chosenClass = "Assassin"; break;
+	default: std::cout << "Invalid Class!" << std::endl;
 	}
+
 	std::cout << "You chose " << chosenClass << "!" << std::endl;
 	return chosenClass;
 }
 
-void World::printCombatUI(std::string playerName, int pHp, int pAtk, int eHp, int eAtk, int creds){
+void World::printCombatUI(std::string playerName, int pHp, int pAtk, int eHp, int eAtk, int creds) {
 	std::cout << "The Jimbob Paradox" << std::endl;
 	std::cout << "Credits: " << creds << std::endl;
 	std::cout << playerName << " Health: " << pHp << std::endl;
 	std::cout << "Attack: " << pAtk << std::endl;
 	std::cout << std::endl;
-	std::cout <<"Enemy Health: " << eHp << std::endl; 
+	std::cout << "Enemy Health: " << eHp << std::endl;
 	std::cout << "Attack: " << eAtk << std::endl;
 
-	grid[6][7] = 'x'; // dw about this im just test running
+	grid[6][7] = 'x';
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			std::cout << grid[i][j] << ' ';
 		}
 		std::cout << std::endl;
 	}
-
 }
 
 void World::earnCredits(std::string enemyType) {
