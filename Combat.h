a3be4838e11e6bec2505b73a0d6edb3345e0ba6d
naylisophillea <naylisophillea@gmail.com>
2026-08-20 20:36:45 +0800

    Update Combat.h 20/8 9pm

--- Combat.h
+++ Combat.h
@@ -4,17 +4,16 @@
 
 class Combat {
 public:
-	// pClassName should be "Fairy", "Witch", "Assassin", or "" for no gimmick
 	Combat(Character& player, Character& enemy, std::string pClassName = "");
+
+	void applyGimmick();
+	void tickDotEffects();     
+	void doEnemyTurn();
 
 private:
 	Character& player;
 	Character& enemy;
 	std::string pClassName;
-
-	// enemy gimmick flags, worked out from enemy.getName() in the constructor:
-	//   isExplodeEnemy - Bombji / boss Bobmij: attack deals bonus "explosion" damage
-	//   isDotEnemy     - Bomjib / Jimbob: attack also applies a 3-turn DoT to the player
 	bool isExplodeEnemy;
 	bool isDotEnemy;
 
@@ -23,10 +22,8 @@ private:
 	int enemyDotTurnsLeft;   // DoT ticking on the player, from Bomjib/Jimbob
 	int fairyTurnCounter;    // player turns taken since last Fairy heal
 
-	int getEnemyAttackDamage(); // handles the explode gimmick's bonus damage
+	int getEnemyAttackDamage(); 
 	void healCharacter(Character& target, int amount); // caps at maxHp, no heal() on Character yet
 
-	void tickDotEffects();     // applies + decrements any active DoTs at the start of a round
-	bool doPlayerTurn();       // returns false if the player chose to flee
-	void doEnemyTurn();
+	
 };
