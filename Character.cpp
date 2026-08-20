66b567b595af406730bf98495bd625ccfed4245e
naylisophillea <naylisophillea@gmail.com>
2026-08-20 14:54:46 +0800

    Update Character.cpp 20/8 3pm

--- Character.cpp
+++ Character.cpp
@@ -1,9 +1,8 @@
 #include "Character.h"
 
 Character::Character(std::string name, int hp, int attack)
-	: name(name), hp(hp), maxHp(hp), attack(attack) {
-};
-
+	: name(name), hp(hp), maxHp(hp), atk(attack) { 
+}
 bool Character::isAlive() {
 	return hp > 0;
 }
@@ -14,13 +13,13 @@ void Character::takeDamage(int amount) {
 }
 
 int Character::getAttack() {
-	return attack;
+	return atk;
 }
 
 std::string Character::getName() {
 	return name;
 }
-
+  
 int Character::getHp() {
 	return hp;
 }
