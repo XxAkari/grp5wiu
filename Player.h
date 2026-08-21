8ae42d55984e7012de52546a29c8c4f08b9600fc
naylisophillea <naylisophillea@gmail.com>
2026-08-21 09:43:03 +0800

    Update Player.h 9am 21/8

--- Player.h
+++ Player.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "Character.h"
-class Player :
-    public Character
-{
-};
 
+class Player : public Character {
+public:
+	std::string name;
+	int hp;
+	int atk;
+
+	Player(std::string name, int hp, int attack);
+};
+ 
