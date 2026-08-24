46aaf03b21e6e51bab2f9584a1ba31e874014f97
summerpaigetan <summerpaigetan@gmail.com>
2026-08-24 15:31:05 +0800

    Update Combat.h

--- Combat.h
+++ Combat.h
@@ -1,29 +1,31 @@
+
 #pragma once
 #include "Character.h"
 #include <string>
 
 class Combat {
 public:
-	Combat(Character& player, Character& enemy, std::string pClassName = "");
+    Combat(Character& player, Character& enemy, std::string pClassName = "");
 
-	void applyGimmick();
-	void tickDotEffects();     
-	void doEnemyTurn();
+    void applyGimmick();
+    void tickDotEffects();
+    void doEnemyTurn();
 
 private:
-	Character& player;
-	Character& enemy;
-	std::string pClassName;
-	bool isExplodeEnemy;
-	bool isDotEnemy;
+    Character& player;
+    Character& enemy;
+    std::string pClassName;
+    bool isExplodeEnemy;
+    bool isDotEnemy;
 
-	// gimmick state trackers
-	int witchDotTurnsLeft;   // DoT ticking on the enemy, from Witch's attack
-	int enemyDotTurnsLeft;   // DoT ticking on the player, from Bomjib/Jimbob
-	int fairyTurnCounter;    // player turns taken since last Fairy heal
+    // gimmick state trackers
+    int witchDotTurnsLeft;   // DoT ticking on the enemy, from Witch's attack
+    int enemyDotTurnsLeft;   // DoT ticking on the player, from Bomjib/Jimbob
+    int explosionCooldown;
+    int fairyTurnCounter;    // player turns taken since last Fairy heal
 
-	int getEnemyAttackDamage(); 
-	void healCharacter(Character& target, int amount); // caps at maxHp, no heal() on Character yet
+    int getEnemyAttackDamage();
+    void healCharacter(Character& target, int amount); // caps at maxHp, no heal() on Character yet
 
-	
+
 };
