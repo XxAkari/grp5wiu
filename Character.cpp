227c7e670834180ea20ceadfeb174f0acf65ef52
naylisophillea <naylisophillea@gmail.com>
2026-08-25 21:22:05 +0800

    Update Character.cpp 25/8 9pm

--- Character.cpp
+++ Character.cpp
@@ -1,10 +1,24 @@
 #include "Character.h"
 
+Character::Character() {}
+
 Character::Character(std::string name, int hp, int attack)
-	: name(name), hp(hp), maxHp(hp), atk(attack) { 
+	: name(name), hp(hp), maxHp(hp), atk(attack) {
 }
 bool Character::isAlive() {
 	return hp > 0;
+}
+
+void Character::setAttack(int atk) {
+	this->atk = atk; 
+}
+
+void Character::setHeaalth(int health) {
+	hp = health;
+}
+
+void Character::setMaxHP(int maxHP) {
+	maxHp = maxHP;
 }
 
 void Character::takeDamage(int amount) {
@@ -19,11 +33,23 @@ int Character::getAttack() {
 std::string Character::getName() {
 	return name;
 }
-  
+
 int Character::getHp() {
 	return hp;
 }
 
 int Character::getMaxHp() {
 	return maxHp;
+}
+
+void Character::addHp(int hp) {
+	this->hp += hp;
+}
+
+void Character::addMaxHP(int hp) {
+	maxHp += hp;
+}
+
+void Character::addAttack(int atk) {
+	this->atk += atk; 
 }
