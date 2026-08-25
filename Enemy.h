ac300e2d80903662764ccd799245a2ff4d083e61
summerpaigetan <summerpaigetan@gmail.com>
2026-08-25 09:53:30 +0800

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,11 +1,20 @@
 #pragma once
+#include <string>
 #include "Character.h"
 
-// TEMPORARY placeholder Enemy class, just so combat can be tested with
 class Enemy : public Character {
 public:
+
+	//constructor
 	Enemy(std::string name, int hp, int attack);
 
-	// makes this enemy hit target for its own attack stat
-	void attack(Character& target);
+	//attack function
+	void atk(Character& target);
+
+	//enemy names
+	void BombjiName();
+	void BomjibName();
+	void MobijName();
+
+	void printEnemyName();
 };
