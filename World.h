da5bb9bbf0bd8297dc474748f9c38fe026af3095
naylisophillea <naylisophillea@gmail.com>
2026-08-25 21:26:52 +0800

    Update World.h 25/8 9pm

--- World.h
+++ World.h
@@ -1,22 +1,20 @@
 #include <string>
-
 #pragma once
 class World
 {
 private:
-	// char grid[10][10];
 	int currentLevel;
 	int credits;
 	int creditsRewarded;
-
 public:
 	World();
-	int getCredits(); // accessor to credits value, not the earning one
+	int getCredits(); // accessor to credits value
 	int getCurrentLevel();
 	int changeLevel(); // level increment
 	std::string namingUI();
 	std::string playerClassUI();
-
 	void earnCredits(std::string enemyType);
-	void printCombatUI(std::string playerName, int pHp, int pAtk, int eHp, int eAtk, int creds);
+	void printCombatUI(std::string playerName, int pHp, int pAtk, int eHp, int eAtk, int creds, int turnCounter, std::string statusEffects);
+	void shopASCIIprint();
+	void printShopUI();
 };
