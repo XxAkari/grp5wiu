9a1c080a10c229417f81b722ea08c7b8e97b98d4
naylisophillea <naylisophillea@gmail.com>
2026-08-27 11:01:09 +0800

    Delete Enemy.cpp

--- Enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "Enemy.h"
-
-Enemy::Enemy(std::string name, int hp, int attack)
-	: Character(name, hp, attack) {
-}
-
-void Enemy::attack(Character& target) {
-	target.takeDamage(getAttack());
-}
