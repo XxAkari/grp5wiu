e41e32f8e5cb65598199b711e433064c01687d07
summerpaigetan <summerpaigetan@gmail.com>
2026-08-27 16:25:27 +0800

    Update Combat.h

--- Combat.h
+++ Combat.h
@@ -1,29 +1,28 @@
 #pragma once
 #include "Character.h"
 #include <string>
-
 class Combat {
 public:
-	Combat(Character& player, Character& enemy, std::string pClassName = "");
+    Combat(Character& player, Character& enemy, std::string pClassName = "");
+    void applyGimmick();
+    void tickDotEffects();
+    void doEnemyTurn();
+    void explodeASCII();
+    void printExplodeASCII();
 
-	void applyGimmick();
-	void tickDotEffects();     
-	void doEnemyTurn();
-
+    int getWitchDotTurnsLeft() const;
+    int getEnemyDotTurnsLeft() const;
+    int getTurnsUntilFairyHeal() const;
 private:
-	Character& player;
-	Character& enemy;
-	std::string pClassName;
-	bool isExplodeEnemy;
-	bool isDotEnemy;
-
-	// gimmick state trackers
-	int witchDotTurnsLeft;   // DoT ticking on the enemy, from Witch's attack
-	int enemyDotTurnsLeft;   // DoT ticking on the player, from Bomjib/Jimbob
-	int fairyTurnCounter;    // player turns taken since last Fairy heal
-
-	int getEnemyAttackDamage(); 
-	void healCharacter(Character& target, int amount); // caps at maxHp, no heal() on Character yet
-
-	
+    Character& player;
+    Character& enemy;
+    std::string pClassName;
+    bool isExplodeEnemy;
+    bool isDotEnemy;
+    int witchDotTurnsLeft;
+    int enemyDotTurnsLeft;
+    int explosionCooldown;
+    int fairyTurnCounter;
+    int getEnemyAttackDamage();
+    void healCharacter(Character& target, int amount);
 };
