19de2038e9cd28479604876b7320b06fa681ae33
summerpaigetan <summerpaigetan@gmail.com>
2026-08-27 16:25:00 +0800

    Update Character.h

--- Character.h
+++ Character.h
@@ -3,7 +3,10 @@
 
 class Character {
 public:
+	Character();
+
 	Character(std::string name, int hp, int attack);
+	Character(std::string name, int hp, int maxHp, int attack); // lets a subclass (Enemy) set starting hp and max hp separately
 
 	bool isAlive();
 	void takeDamage(int amount);
@@ -11,6 +14,14 @@ public:
 	std::string getName();
 	int getHp();
 	int getMaxHp();
+
+	void setAttack(int atk);
+	void setHealth(int health);
+	void setMaxHP(int maxHP);
+
+	void addHp(int hp);
+	void addMaxHP(int hp);
+	void addAttack(int atk);
 
 private:
 	std::string name;
