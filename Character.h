aa1bcb12a099a4b04b8f46f7e9add23d135069d3
summerpaigetan <summerpaigetan@gmail.com>
2026-08-27 14:58:44 +0800

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,31 +1,28 @@
 #pragma once
+#include "Character.h"
 #include <string>
-
-class Character {
+class Combat {
 public:
-	Character();
+    Combat(Character& player, Character& enemy, std::string pClassName = "");
+    void applyGimmick();
+    void tickDotEffects();
+    void doEnemyTurn();
+    void explodeASCII();
+    void printExplodeASCII();
 
-	Character(std::string name, int hp, int attack);
-	Character(std::string name, int hp, int maxHp, int attack); // lets a subclass (Enemy) set starting hp and max hp separately
-
-	bool isAlive();
-	void takeDamage(int amount);
-	int getAttack();
-	std::string getName();
-	int getHp();
-	int getMaxHp();
-
-	void setAttack(int atk);
-	void setHealth(int health);
-	void setMaxHP(int maxHP);
-
-	void addHp(int hp);
-	void addMaxHP(int hp);
-	void addAttack(int atk);
-
+    int getWitchDotTurnsLeft() const;
+    int getEnemyDotTurnsLeft() const;
+    int getTurnsUntilFairyHeal() const;
 private:
-	std::string name;
-	int hp;
-	int maxHp;
-	int atk;
+    Character& player;
+    Character& enemy;
+    std::string pClassName;
+    bool isExplodeEnemy;
+    bool isDotEnemy;
+    int witchDotTurnsLeft;
+    int enemyDotTurnsLeft;
+    int explosionCooldown;
+    int fairyTurnCounter;
+    int getEnemyAttackDamage();
+    void healCharacter(Character& target, int amount);
 };
