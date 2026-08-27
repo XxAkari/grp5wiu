d4272a2dc9a95ac62198390694c040f20954da9b
summerpaigetan <summerpaigetan@gmail.com>
2026-08-27 16:24:49 +0800

    Update Boss.h

--- Boss.h
+++ Boss.h
@@ -2,19 +2,19 @@
 #include "Enemy.h"
 class Boss : public Enemy
 {
+public:
 	//Constructor
-	Boss();
-
+	Boss(std::string name, int hp, int attack);
 	//Destructor
 	~Boss();
-
 	//Getter
 	int getHP();
 	int getATK();
-
 	//Setter
 	void setHP(int newHP);
 	void setATK(int newATK);
 
+	void ObobjibName();
+	void BobmijName();
+	void JimbobName();
 };
-
